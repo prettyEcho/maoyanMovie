5461c4c4ad8149e4fb57192064cbf6f1b02513d0
prettyEcho <15732029828@163.com>
2018-04-18 09:55:48 +0800

    backend prod config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,8 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  PHOST: '"http://localhost:3000/proxy"',  // proxy host
+  DHOST: '"http://localhost:3000/db/user"', // db host
+  FHOST: '"http://localhost:3000/file/user"', // file host
+  CONTROL_HOST: '"db"'  // db or file
 }
