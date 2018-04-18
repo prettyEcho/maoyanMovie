0facd683a6b8707c4addd31ce4bde2f1601e86e7
prettyEcho <15732029828@163.com>
2018-04-18 09:55:20 +0800

    backend env config

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,9 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  PHOST: '"http://localhost:3000/proxy"',  // proxy host
+  DHOST: '"http://localhost:3000/db/user"', // db host
+  FHOST: '"http://localhost:3000/file/user"', // file host
+  CONTROL_HOST: '"db"'  // db or file
 })
