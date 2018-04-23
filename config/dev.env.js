18209e86656aadcedc65de9ae742dcabcc908101
prettyEcho <15732029828@163.com>
2018-04-23 18:02:37 +0800

    config

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   PHOST: '"http://localhost:3000/proxy"',  // proxy host
-  DHOST: '"http://localhost:3000/db/user"', // db host
-  FHOST: '"http://localhost:3000/file/user"', // file host
+  DHOST: '"http://localhost:3000/duser"', // db host
+  FHOST: '"http://localhost:3000/fuser"', // file host
   MODE: '"db"'  // db or file
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   PHOST: '"http://localhost:3000/proxy"',  // proxy host
-  DHOST: '"http://localhost:3000/db/user"', // db host
-  FHOST: '"http://localhost:3000/file/user"', // file host
+  DHOST: '"http://localhost:3000/duser"', // db host
+  FHOST: '"http://localhost:3000/fuser"', // file host
   MODE: '"db"'  // db or file
 }
