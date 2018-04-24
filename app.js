fd207e5828d68de8595280928c7b1056b7172b97
prettyEcho <15732029828@163.com>
2018-04-24 22:24:20 +0800

    支持跨域cookie

--- app.js
+++ app.js
@@ -40,7 +40,9 @@ app.engine('html', require('hbs').__express);
 
 // 跨域
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  debug(req.hostname);
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header('Access-Control-Allow-Headers', 'Content-Type');
