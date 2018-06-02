b4176857bfe29078610f200527150a8c16f3c824
prettyEcho <15732029828@163.com>
2018-06-02 17:59:25 +0800

    增加footer z-index

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -41,69 +41,70 @@ export default {
     change (index) {
       let path = this.footers[index]['icon']
       this.nowPage = index + 1
-      this.$router.push({ path: path})
+      this.$router.push({ path: path })
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-  @import "../../style/base";
+@import '../../style/base';
 
-  .footer{
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    padding: 25 / @rem 0;
-    height: 180 / @rem;
-    background-color: @body;
-    border-top: 1px solid @grey;
-    .clearfix;
-    .section{
-      float: left;
-      width: 33.333%;
-      height: 130 / @rem;
-      .center{
-        margin: 0 auto;
+.footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 25 / @rem 0;
+  height: 180 / @rem;
+  background-color: @body;
+  border-top: 1px solid @grey;
+  z-index: 9999;
+  .clearfix;
+  .section {
+    float: left;
+    width: 33.333%;
+    height: 130 / @rem;
+    .center {
+      margin: 0 auto;
+      width: 70 / @rem;
+      .top {
         width: 70 / @rem;
-        .top{
-          width: 70 / @rem;
-          height: 70 / @rem;
-        }
-        .bottom{
-          padding-top: 10 / @rem;
-          font-size: 35 / @rem;
-          color: #949494;
-        }
+        height: 70 / @rem;
       }
-      .red{
-        color: #f0423b !important;
-      }
-      .hot{
-        background: url('../../images/sprite.png') no-repeat -70 / @rem 0;
-        background-size: 280 / @rem 210 / @rem;
-      }
-      .hot-now{
-        background: url('../../images/sprite.png') no-repeat 0 0;
-        background-size: 280 / @rem 210 / @rem;
-      }
-      .theatre{
-        background: url('../../images/sprite.png') no-repeat 0 -70/@rem;
-        background-size: 280 / @rem 210 / @rem;
-      }
-      .theatre-now{
-        background: url('../../images/sprite.png') no-repeat -210 / @rem 0;
-        background-size: 280 / @rem 210 / @rem;
-      }
-      .user{
-        background: url('../../images/sprite.png') no-repeat -140 / @rem -70 / @rem;
-        background-size: 280 / @rem 210 / @rem;
-      }
-      .user-now{
-        background: url('../../images/sprite.png') no-repeat  -70 / @rem -70 / @rem;
-        background-size: 280 / @rem 210 / @rem;
+      .bottom {
+        padding-top: 10 / @rem;
+        font-size: 35 / @rem;
+        color: #949494;
       }
     }
+    .red {
+      color: #f0423b !important;
+    }
+    .hot {
+      background: url('../../images/sprite.png') no-repeat -70 / @rem 0;
+      background-size: 280 / @rem 210 / @rem;
+    }
+    .hot-now {
+      background: url('../../images/sprite.png') no-repeat 0 0;
+      background-size: 280 / @rem 210 / @rem;
+    }
+    .theatre {
+      background: url('../../images/sprite.png') no-repeat 0 -70 / @rem;
+      background-size: 280 / @rem 210 / @rem;
+    }
+    .theatre-now {
+      background: url('../../images/sprite.png') no-repeat -210 / @rem 0;
+      background-size: 280 / @rem 210 / @rem;
+    }
+    .user {
+      background: url('../../images/sprite.png') no-repeat -140 / @rem -70 /
+        @rem;
+      background-size: 280 / @rem 210 / @rem;
+    }
+    .user-now {
+      background: url('../../images/sprite.png') no-repeat -70 / @rem -70 / @rem;
+      background-size: 280 / @rem 210 / @rem;
+    }
   }
-
+}
 </style>
